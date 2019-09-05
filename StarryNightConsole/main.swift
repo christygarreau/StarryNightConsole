9b9bc0ab981700483947322eaac4212caed2536c
christygarreau <20cgarreau@castilleja.org>
2019-09-05 09:36:36 -0700

    Initial Commit

---
+++ StarryNightConsole/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StarryNightConsole
+//
+//  Created by Christy Garreau on 9/5/19.
+//  Copyright © 2019 Christy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
